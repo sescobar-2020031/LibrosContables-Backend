34415994f5577da6ef2dbeee7914298de3c66e22
kgarcia2020050 <kgarcia-2020050@kinal.edu.gt>
2023-05-29 18:56:55 -0600

    Editar y eliminar

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -1,38 +1,81 @@
-'use strict'
+"use strict";
 
-const Account = require('../models/account.model');
-const User = require('../models/user.model');
+const Account = require("../models/account.model");
+const User = require("../models/user.model");
 
 exports.saveAccount = async (req, res) => {
-    try {
-        const params = req.body
+  try {
+    const params = req.body;
 
-        const user = await User.findOne({ email: req.user.email });
-        if (!user) return res.status(500).send({ message: 'Usuario no encontrada' });
+    const user = await User.findOne({ email: req.user.email });
+    if (!user)
+      return res.status(500).send({ message: "Usuario no encontrada" });
 
-        const data = {
-            name: params.name,
-            user: user._id
-        }
+    const data = {
+      name: params.name,
+      user: user._id,
+    };
 
-        let account = new Account(data);
-        await account.save();
+    let account = new Account(data);
+    await account.save();
 
-        if (!account) return res.status(500).send({ message: 'No se pudo guardar la cuenta' });
-        return res.status(200).send({ message: 'Cuenta guardada Exitosamente' });
-    } catch (err) {
-        console.log(err);
-    }
-}
+    if (!account)
+      return res.status(500).send({ message: "No se pudo guardar la cuenta" });
+    return res.status(200).send({ message: "Cuenta guardada Exitosamente" });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 exports.getAccounts = async (req, res) => {
+  try {
+    const params = req.user;
+    const user = await User.findOne({ email: params.email });
+    if (!user) return res.status(500).send({ message: "Cuenta no encontrada" });
+    let accounts = await Account.find({ user: user._id });
+    return res
+      .status(200)
+      .send({ message: "Cuentas Encontradas Exitosamente", accounts });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.editAccount = async (req, res) => {
+  try {
+    let account = await Account.find({ _id: req.body.id });
+    if (!account)
+      return res
+        .status(500)
+        .send({ message: "No se pudo modificar la cuenta la cuenta" });
+    Account.updateOne({ _id: req.body.id }, { name: req.body.name })
+      .then(() => {
+        res.status(200).send({ message: "Cuenta actualizada exitosamente." });
+      })
+      .catch((err) => {
+        res.status(500).send({ message: "No se pudo actualizar la cuenta." });
+      });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
+exports.deleteAccount = async (req, res) => {
     try {
-        const params = req.user
-        const user = await User.findOne({ email: params.email });
-        if (!user) return res.status(500).send({ message: 'Cuenta no encontrada' });
-        let accounts = await Account.find({ user: user._id });
-        return res.status(200).send({ message: 'Cuentas Encontradas Exitosamente', accounts });
+      let account = await Account.find({ _id: req.body.id });
+      if (!account)
+        return res.status(500).send({ message: "No se pudo eliminar la cuenta" });
+      Account.deleteOne({ _id: req.body.id })
+        .then(() => {
+          res.status(200).send({ message: "Cuenta eliminada exitosamente" });
+        })
+        .catch((err) => {
+          res.status(500).send({ message: "No se pudo eliminar la cuenta" });
+        });
     } catch (err) {
-        console.log(err);
+      console.log(err);
     }
-}
+  };
+  
+  
--- src/routes/account.route.js
+++ src/routes/account.route.js
@@ -1,12 +1,14 @@
-'use strict'
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const api = express.Router();
 
-const account = require('../controllers/Account.controller');
-const mdAuth = require('../middlewares/authenticated');
+const account = require("../controllers/Account.controller");
+const mdAuth = require("../middlewares/authenticated");
 
-api.post('/addAccount', mdAuth.ensureAuth, account.saveAccount);
-api.get('/getAccount', mdAuth.ensureAuth, account.getAccounts);
+api.post("/addAccount", mdAuth.ensureAuth, account.saveAccount);
+api.get("/getAccount", mdAuth.ensureAuth, account.getAccounts);
+api.post("/editAccount", mdAuth.ensureAuth, account.editAccount);
+api.post("/deleteAccount", mdAuth.ensureAuth, account.deleteAccount);
 
 module.exports = api;
